1e65ec3d9e3f46f096aae10f38143d1982be9f51
Gouda Ananya <ananyagouda25@gmail.com>
2026-08-27 22:48:34 +0530

    Add binary search implementation for square root calculation
    Implemented a binary search algorithm to calculate the square root of a number without using built-in functions.

---
+++ sqrt(x)_(LeetCode #69).py
@@ -0,0 +1,19 @@
+# Program: It calculates the square root of a number without using the existing mathematical functions.
+# Concepts: Binary search
+
+class Solution:
+    def mySqrt(self,x:int)-> int:
+        low=0
+        high=x
+        while low<=high:
+            mid=(low+high)//2
+            if (mid*mid)==x:
+                return mid
+            elif (mid*mid)<x:
+                low=mid+1
+            else:
+                high=mid-1
+        return high
+sol=Solution()
+a=int(input())
+print(sol.mySqrt(a))
