16fb8f52a853370b5dacab86e06f8cfd868db2c0
Gouda Ananya <ananyagouda25@gmail.com>
2026-09-03 23:12:06 +0530

    Add FizzBuzz implementation in Python
    Implement FizzBuzz solution for LeetCode problem 412.

---
+++ #412_fizz_buzz_(LeetCode #412).py
@@ -0,0 +1,20 @@
+# Program: LeetCode-412: FizzBuzz
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        a=[]
+        for i in range(1,n+1):
+            if i%15==0:
+                a.append("FizzBuzz")
+            elif i%3==0:
+                a.append("Fizz")
+            elif i%5==0:
+                a.append("Buzz")
+            else:
+                a.append(str(i))
+        return a
+s=Solution()
+n=int(input("n = "))
+print(s.fizzBuzz(n))
